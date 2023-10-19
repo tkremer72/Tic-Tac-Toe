d90307b424171abbc901ad3e7e8d7e907ec2357f
tkremer <tbone7243@gmail.com>
2023-10-18 20:16:13 -0400

    Second commit, updated the app such that the board keeps track of the state of the 9 squares.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 
 
-
-function Square({ value }) {
+function Square() {
+  const [ value, setValue ] = useState(null);
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
   return (
     <button className="square" onClick={handleClick}>{value}</button>
@@ -11,22 +12,23 @@ function Square({ value }) {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
   <div className="board-row">
-    <Square value="1" />
-    <Square value="2" />
-    <Square value="3" />
+    <Square value={squares[0]}/>
+    <Square value={squares[1]} />
+    <Square value={squares[2]} />
   </div>
   <div className="board-row">
-  <Square value="4" />
-  <Square value="5" />
-  <Square value="6" />
+  <Square value={squares[4]} />
+  <Square value={squares[5]} /> 
+  <Square value={squares[6]}/>
   </div>
   <div className="board-row">
-  <Square value="7" />
-  <Square value="8" />
-  <Square value="9" />
+  <Square value={squares[7]} />
+  <Square value={squares[8]} />
+  <Square value={squares[9]} />
   </div>
 
     </>
